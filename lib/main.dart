adae5d184b16a1b414ee25b5f01ea37631b441a7
Muhammad Umer Adeem <adeemumer800@gmail.com>
2021-05-12 15:41:14 +0500

    Work Complete

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Muhammad Umer Adeem',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
         body: Text(" Name : Muhammad Umer Adeem \n Email : adeemumer800@gmail.com"),
       ),
